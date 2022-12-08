5661a9eaff15d80bf49beabec6f5315bd82d43ff
もちづきひろき <>
2022-12-08 10:46:41 +0900

    カテゴリー削除機能実装　修正　取り消し

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -6,6 +6,11 @@ export default {
     categories: [],
     errorMessage: '',
     doneMessage: '',
+<<<<<<< HEAD
+=======
+    deleteCategoryId: null,
+    deleteCategoryName: '',
+>>>>>>> parent of b70deba (カテゴリー削除機能実装　修正)
     isLoading: false,
   },
   mutations: {
@@ -62,5 +67,28 @@ export default {
     clearMessage({ commit }) {
       commit('clearMessage');
     },
+<<<<<<< HEAD
+=======
+    modalDeleteCategory({ commit }, { categoryId, categoryName }) {
+      commit('modalDeleteCategory', { categoryId, categoryName });
+    },
+    deleteCategory({ commit, rootGetters, state }) {
+      return new Promise(resolve => {
+        commit('clearMessage');
+        const data = new URLSearchParams();
+        data.append('id', state.deleteCategoryId);
+        axios(rootGetters['auth/token'])({
+          method: 'DELETE',
+          url: `/category/${state.deleteCategoryId}`,
+          data,
+        }).then(() => {
+          commit('doneMessage', { message: 'カテゴリーを削除しました' });
+          resolve();
+        }).catch(err => {
+          commit('errorMessage', err.message);
+        });
+      });
+    },
+>>>>>>> parent of b70deba (カテゴリー削除機能実装　修正)
   },
 };
