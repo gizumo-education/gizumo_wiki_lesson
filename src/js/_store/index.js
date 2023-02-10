e6cae805734ad879dd6546a99851bd466eb9f832
Onodera Yuki <ondryuki@gmail.com>
2023-02-10 10:36:59 +0900

    add: modules > index.js にてimportとexportにcategoriesを追加

--- src/js/_store/index.js
+++ src/js/_store/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import {
-  auth, articles, users,
+  auth, articles, users, categories,
 } from './modules';
-import categories from './modules/categories';
 
 Vue.use(Vuex);
 export default new Vuex.Store({
