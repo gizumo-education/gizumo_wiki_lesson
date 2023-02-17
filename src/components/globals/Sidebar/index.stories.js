c288bf5f78adb2f3b9e4beca9807956f27c12d35
吉村優芽 <yumeyoshimura@yoshimurayumenoMacBook-Air.local>
2023-02-17 17:26:48 +0900

    カテゴリー管理画面の実装 routerLinkArrayの修正

--- src/components/globals/Sidebar/index.stories.js
+++ src/components/globals/Sidebar/index.stories.js
@@ -3,13 +3,34 @@ import storyRouter from 'storybook-vue-router';
 
 import note from './README.md';
 
-const routeLinksArray = [];
+const routeLinksArray = [
+  {
+    id: 1,
+    name: 'HOME',
+    path: '/',
+  },
+  {
+    id: 2,
+    name: '記事',
+    path: '/all/articles',
+  },
+  {
+    id: 3,
+    name: 'カテゴリー',
+    path: '/category',
+  },
+  {
+    id: 4,
+    name: 'ユーザー',
+    path: '/users',
+  },
+]
 
 storiesOf('GLOBALS|Sidebar', module)
   .addDecorator(storyRouter())
   .add('default', () => {
     return {
-      template: `<app-sidebar
+      template: `<app-sidebar 
                   :target-array="routeLinksArray"
                 />`,
       computed: {
