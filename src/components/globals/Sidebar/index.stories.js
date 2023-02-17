d5e3fc251e84a3b6229b12544928d08d3d224be1
吉村優芽 <yumeyoshimura@yoshimurayumenoMacBook-Air.local>
2023-02-17 17:20:39 +0900

    カテゴリー管理画面の実装 差分の修正

--- src/components/globals/Sidebar/index.stories.js
+++ src/components/globals/Sidebar/index.stories.js
@@ -3,28 +3,7 @@ import storyRouter from 'storybook-vue-router';
 
 import note from './README.md';
 
-const routeLinksArray = [
-  // {
-  //   id: 1,
-  //   name: 'HOME',
-  //   path: '/',
-  // },
-  // {
-  //   id: 2,
-  //   name: '記事',
-  //   path: '/all/articles',
-  // },
-  // {
-  //   id: 3,
-  //   name: 'カテゴリー',
-  //   path: '/category',
-  // },
-  // {
-  //   id: 4,
-  //   name: 'ユーザー',
-  //   path: '/users',
-  // },
-]
+const routeLinksArray = [];
 
 storiesOf('GLOBALS|Sidebar', module)
   .addDecorator(storyRouter())
--- src/js/_helpers/routeLinksArray.js
+++ src/js/_helpers/routeLinksArray.js
@@ -20,22 +20,3 @@ export default [
     path: '/users',
   },
 ];
-
-// 初期
-// export default [
-//   {
-//     id: 1,
-//     name: 'HOME',
-//     path: '/',
-//   },
-//   {
-//     id: 3,
-//     name: '記事',
-//     path: '/articles',
-//   },
-//   {
-//     id: 4,
-//     name: 'ユーザー',
-//     path: '/users',
-//   },
-// ];
