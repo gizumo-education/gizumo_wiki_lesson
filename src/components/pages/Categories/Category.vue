ea16b2133b71b0cfb7a083d451f0ba27b74f4da5
金井紗理奈 <kanaisarina@kanaisarinanoMacBook-Air.local>
2023-02-22 15:31:39 +0900

    add:カテゴリー新規作成機能の追加

--- src/components/pages/Categories/Category.vue
+++ src/components/pages/Categories/Category.vue
@@ -4,11 +4,17 @@
       <app-category-post
         :access="access"
         :error-message="errorMessage"
+        :done-message="doneMessage"
+        :category="targetCategory.name"
+        @update-value="$event =>targetCategory.name = $event.target.value"
+        @clear-message="clearMessage"
+        @handle-submit="createCategory"
       />
     </div>
     <div class="category-list-pages">
       <app-category-list
         :categories="categoryList"
+        class="category__list"
         :theads="theads"
         :access="access"
       />
@@ -27,6 +33,9 @@ export default {
   data() {
     return {
       theads: ['カテゴリー名'],
+      targetCategory: {
+        name: '',
+      },
     };
   },
   computed: {
@@ -36,12 +45,27 @@ export default {
     errorMessage() {
       return this.$store.state.categories.errorMessage;
     },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
     categoryList() {
       return this.$store.state.categories.categoryList;
     },
   },
   created() {
     this.$store.dispatch('categories/getAllCategories');
+  },
+  methods: {
+    clearMessage() {
+      this.$store.dispatch('categories/clearMessage');
+    },
+    createCategory() {
+      if (this.loading) return;
+      this.$store.dispatch('categories/createCategory', {
+        /* eslint-disable-next-line no-irregular-whitespace */
+        name: this.targetCategory.name.replace(/( |　)+/, '').trim(),
+      });
+    },
   },
 };
 </script>
--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -3,19 +3,39 @@ import axios from '@Helpers/axiosDefault';
 export default {
   namespaced: true,
   state: {
+    loading: false,
     categoryList: [],
     errorMessage: '',
+    doneMessage: '',
+    category: {
+      id: null,
+      name: '',
+    },
   },
 
   mutations: {
+    clearMessage(state) {
+      state.errorMessage = '';
+      state.doneMessage = '';
+    },
     doneGetAllCategories(state, { categories }) {
       state.categoryList = categories.reverse();
     },
     failRequest(state, { message }) {
       state.errorMessage = message;
     },
+    applyRequest(state) {
+      state.loading = true;
+    },
+    doneCreateCategory(state) {
+      state.loading = false;
+      state.doneMessage = '新規カテゴリーの追加が完了しました。';
+    },
   },
   actions: {
+    clearMessage({ commit }) {
+      commit('clearMessage');
+    },
     // カテゴリー全件取得
     getAllCategories({ commit, rootGetters }) {
       axios(rootGetters['auth/token'])({
@@ -30,6 +50,23 @@ export default {
         commit('doneGetAllCategories', { categories });
       }).catch(err => {
         commit('failRequest', { message: err.message });
+      });
+    },
+    // カテゴリー新規作成
+    createCategory({ commit, rootGetters }, category) {
+      commit('applyRequest');
+      return new Promise(resolve => {
+        axios(rootGetters['auth/token'])({
+          method: 'POST',
+          url: '/category',
+          data: category,
+        }).then(response => {
+          if (response.data.code === 0) throw new Error(response.data.message);
+          commit('doneCreateCategory');
+          resolve();
+        }).catch(err => {
+          commit('failRequest', { message: err.response.data.message });
+        });
       });
     },
   },
