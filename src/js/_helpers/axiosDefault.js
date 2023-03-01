267195629554f771b7f587a155b48e51b0d39e06
Joex13 <aokijo0113p@gmail.com>
2023-03-01 17:41:17 +0900

    refactor: eslintによる整形&不要なコメントの削除

--- src/js/_helpers/axiosDefault.js
+++ src/js/_helpers/axiosDefault.js
@@ -4,9 +4,9 @@ export default (token = null) => {
   const headers = {};
   if (token) Object.assign(headers, { Authorization: `Bearer ${token}` });
 
-  // インスタンスの作成（戻り値の関数を実行するとAPI通信できるようだ）
+  // インスタンスの作成（戻り値の関数を実行するとAPI通信できる）
   return axios.create({
-    baseURL: API_BASE_URL, // ←なにこれ
+    baseURL: API_BASE_URL,
     headers,
   });
 };
