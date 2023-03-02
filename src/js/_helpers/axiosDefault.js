d071fafc624b2b42500499687a268e90da73cb2f
Joex13 <aokijo0113p@gmail.com>
2023-03-02 11:15:44 +0900

    delete: 不要な記述の削除
    コメントアウト/おま環解決コードの除去

--- src/js/_helpers/axiosDefault.js
+++ src/js/_helpers/axiosDefault.js
@@ -4,7 +4,6 @@ export default (token = null) => {
   const headers = {};
   if (token) Object.assign(headers, { Authorization: `Bearer ${token}` });
 
-  // インスタンスの作成（戻り値の関数を実行するとAPI通信できる）
   return axios.create({
     baseURL: API_BASE_URL,
     headers,
