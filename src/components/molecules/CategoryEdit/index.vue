e6dd37d41dbd2bb621ebd2011eabe35a55ad5755
kanai-sarina <k.s3192@icloud.com>
2023-03-06 17:05:53 +0900

    fix：不必要なコメントを削除

--- src/components/molecules/CategoryEdit/index.vue
+++ src/components/molecules/CategoryEdit/index.vue
@@ -71,10 +71,6 @@ export default {
       type: Object,
       default: () => ({}),
     },
-    // editTargetCategoryName: {
-    //   type: String,
-    //   default: '',
-    // },
   },
   computed: {
     buttonText() {
