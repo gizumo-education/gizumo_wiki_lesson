658cdf3adfb1f3e434a3c672e1a2a29ef93408a3
DESKTOP-S3V5KMT\217g0 <sashimi.travail@gmail.com>
2023-03-18 03:14:24 +0900

    added create new category function

--- src/components/pages/Categories/Categories.vue
+++ src/components/pages/Categories/Categories.vue
@@ -2,9 +2,13 @@
   <div class="category-page">
     <app-category-post
       :access="access"
-      :error-message="errorMessage"
+      :category="category"
       :done-message="doneMessage"
+      :error-message="errorMessage"
+      :loading="loading"
       class="category-page__post"
+      @handle-submit="handleSubmit"
+      @update-value="updateValue"
     />
     <div class="category-page__list">
       <app-category-list
@@ -33,22 +37,38 @@ export default {
     access() {
       return this.$store.getters['auth/access'];
     },
+    category() {
+      const { name } = this.$store.state.categories.category;
+      return name;
+    },
     categoryList() {
       return this.$store.state.categories.categoryList;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
     },
     errorMessage() {
       return this.$store.state.categories.errorMessage;
     },
-    doneMessage() {
-      return this.$store.state.categories.doneMessage;
+    loading() {
+      return this.$store.state.categories.loading;
     },
   },
   created() {
     this.fetchCategories();
   },
   methods: {
+    clearMessage() {
+      this.$store.dispatch('categories/clearMessage');
+    },
     fetchCategories() {
       this.$store.dispatch('categories/getAllCategories');
+    },
+    handleSubmit() {
+      this.$store.dispatch('categories/postCategories');
+    },
+    updateValue($event) {
+      this.$store.dispatch('categories/updateValue', $event.target.value);
     },
   },
 };
--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -3,20 +3,50 @@ import axios from '@Helpers/axiosDefault';
 export default {
   namespaced: true,
   state: {
-    errorMessage: '',
+    category: {
+      id: null,
+      name: '',
+    },
     categoryList: [],
+    errorMessage: '',
+    doneMessage: '',
+    loading: false,
+  },
+  getters: {
+    category: state => state.category,
   },
   mutations: {
+    clearMessage(state) {
+      state.errorMessage = '';
+    },
+    displayDoneMessage(state, payload = { message: '成功しました' }) {
+      state.doneMessage = payload.message;
+    },
     doneGetAllCategories(state, payload) {
       state.categoryList = [...payload.categories];
     },
     failRequest(state, { message }) {
       state.errorMessage = message;
     },
+    initPostCategory(state) {
+      state.category = {
+        id: null,
+        name: '',
+      };
+    },
+    addCategory(state, payload) {
+      state.categoryList.unshift(payload);
+    },
+    toggleLoading(state) {
+      state.loading = !state.loading;
+    },
+    updateValue(state, payload) {
+      state.category = { ...state.category, name: payload.category };
+    },
   },
   actions: {
-    initPostCategory({ commit }) {
-      commit('initPostCategory');
+    clearMessage({ commit }) {
+      commit('clearMessage');
     },
     getAllCategories({ commit, rootGetters }) {
       axios(rootGetters['auth/token'])({
@@ -29,6 +59,39 @@ export default {
         commit('doneGetAllCategories', payload);
       }).catch(err => {
         commit('failRequest', { message: err.message });
+      });
+    },
+    initPostCategory({ commit }) {
+      commit('initPostCategory');
+    },
+    postCategories({ commit, rootGetters }) {
+      return new Promise((resolve, reject) => {
+        commit('clearMessage');
+        commit('toggleLoading');
+        const data = new URLSearchParams();
+        data.append('name', rootGetters['categories/category'].name);
+        axios(rootGetters['auth/token'])({
+          method: 'POST',
+          url: '/category',
+          data,
+        }).then(res => {
+          const payload = res.data.category;
+          commit('addCategory', payload);
+          commit('initPostCategory');
+          commit('toggleLoading');
+          commit('displayDoneMessage', { message: 'カテゴリーを作成しました' });
+          resolve();
+        }).catch(err => {
+          commit('toggleLoading');
+          commit('failRequest', { message: err.message });
+          reject();
+        });
+      });
+    },
+    updateValue({ commit }, category) {
+      commit({
+        type: 'updateValue',
+        category,
       });
     },
   },
