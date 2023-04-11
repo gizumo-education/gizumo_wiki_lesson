0a075e79c837a1b60fa37eff14d0714ce22ed76e
ta9to <ta9to@TakutonoMacBook-Pro.local>
2023-04-11 15:50:10 +0900

    feat:カテゴリー更新画面作成

---
+++ src/components/molecules/CategoryEdit/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="category-edit">
+    <app-heading :level="1">カテゴリー管理</app-heading>
+    <app-router-link
+      underline
+      hover-opacity
+      class="category-edit__back-link"
+    >
+      カテゴリー一覧へ戻る
+    </app-router-link>
+    <app-input
+      v-validate="'required'"
+      name="category"
+      type="text"
+      placeholder="追加するカテゴリー名を入力してください"
+      data-vv-as="カテゴリー名"
+      :error-messages="errors.collect('category')"
+      :value="category"
+      class="category-edit__form"
+      @update-value="$emit('update-value', $event)"
+    />
+    <app-button
+      class="category-edit__submit"
+      button-type="submit"
+      round
+      :disabled="!disabled"
+      @click="handleSubmit"
+    >
+      {{ buttonText }}
+    </app-button>
+    <div v-if="errorMessage" class="category-edit__notice">
+      <app-text bg-error>{{ errorMessage }}</app-text>
+    </div>
+
+    <div v-if="doneMessage" class="category-edit__notice">
+      <app-text bg-success>{{ doneMessage }}</app-text>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  Heading, RouterLink, Input, Button,
+} from '@Components/atoms';
+
+export default {
+  components: {
+    appHeading: Heading,
+    appRouterLink: RouterLink,
+    appInput: Input,
+    appButton: Button,
+  },
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+    access: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    buttonText() {
+      if (!this.access.edit) return '更新権限がありません';
+      return this.loading ? '更新中...' : '更新';
+    },
+    disabled() {
+      return this.access.edit && !this.loading;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .category-edit {
+    &__back-link {
+      margin-top: 16px;
+    }
+    &__form {
+      margin-top: 20px;
+    }
+    &__submit {
+      margin-top: 16px;
+    }
+    &__notice {
+      margin-top: 16px;
+    }
+  }
+</style>
--- src/components/pages/Categories/Edit.vue
+++ src/components/pages/Categories/Edit.vue
@@ -1,3 +1,24 @@
 <template>
-  <div>カテゴリー更新画面（仮）</div>
+  <app-category-edit
+    :loading="loading"
+    :access="access"
+  />
 </template>
+
+<script>
+import { CategoryEdit } from '@Components/molecules';
+
+export default {
+  components: {
+    appCategoryEdit: CategoryEdit,
+  },
+  computed: {
+    loading() {
+      return this.$store.state.categories.loading;
+    },
+    access() {
+      return this.$store.getters['auth/access'];
+    },
+  },
+};
+</script>
