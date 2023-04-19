c8b75b2587ed872a317d5a71981c8b7978c844d5
堀家將平 <shohei@horikenoMacBook-Pro.local>
2023-04-19 11:25:33 +0900

    コンポーネントの階層を修正

--- src/components/pages/Categories/List.vue
+++ src/components/pages/Categories/List.vue
@@ -1,31 +1,57 @@
 <template>
-  <app-category-list
-    :theads="theads"
-    :categories="categories"
-    :delete-category-name="deleteCategoryName"
-    :access="access"
-  />
+  <div class="categories">
+    <div class="categories__post">
+      <app-category-post
+        :category="targetCategory"
+        :error-message="errorMessage"
+        :done-message="doneMessage"
+        :disabled="disabled"
+        :access="access"
+      />
+    </div>
+    <div class="categories__content">
+      <app-category-list
+        :theads="theads"
+        :categories="categories"
+        :delete-category-name="deleteCategoryName"
+        :access="access"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
-import { CategoryList } from '@Components/molecules';
+import { CategoryList, CategoryPost } from '@Components/molecules';
 
 export default {
   components: {
     AppCategoryList: CategoryList,
+    AppCategoryPost: CategoryPost,
   },
   computed: {
-    theads() {
-      return this.$store.state.categories.theads;
+    access() {
+      return this.$store.getters['auth/access'];
     },
     categories() {
       return this.$store.state.categories.categoryList;
     },
-    access() {
-      return this.$store.getters['auth/access'];
-    },
     deleteCategoryName() {
       return this.$store.state.categories.deleteCategoryName;
+    },
+    disabled() {
+      return this.$store.state.categories.disabled;
+    },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    targetCategory() {
+      return this.$store.state.categories.targetCategory;
+    },
+    theads() {
+      return this.$store.state.categories.theads;
     },
   },
   created() {
@@ -34,6 +60,22 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss' scoped>
+  .categories {
+    height: 100%;
+    display: flex;
 
+    &__post {
+      height: 100%;
+      width: 30%;
+      margin-right: 25px;
+    }
+
+    &__content {
+      height: 100%;
+      width: 70%;
+      padding-left: 25px;
+      border-left: 1px solid #cdcdcd;
+    }
+  }
 </style>
