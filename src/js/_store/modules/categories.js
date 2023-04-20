06b6cb550fe6748b97699fb07ecc9aea327fa12f
堀家將平 <shohei@horikenoMacBook-Pro.local>
2023-04-20 15:00:34 +0900

    新規カテゴリ追加　実装

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -3,6 +3,9 @@ import axios from '@Helpers/axiosDefault';
 export default {
   namespaced: true,
   state: {
+    targetCategory: '',
+    errorMessage: '',
+    doneMessage: '',
     categoryList: [],
     disabled: false,
   },
@@ -10,6 +13,25 @@ export default {
     doneGetAllCategories(state, payload) {
       state.categoryList = payload.categories.reverse();
     },
+    updateCategory(state, payload) {
+      state.targetCategory = payload.targetCategory;
+    },
+    clearMessage(state) {
+      state.doneMessage = '';
+      state.errorMessage = '';
+    },
+    toggleLoading(state) {
+      state.disabled = !state.disabled;
+    },
+    failRequest(state, { message }) {
+      state.errorMessage = message;
+    },
+    displayDoneMessage(state, payload = { message: '成功しました' }) {
+      state.doneMessage = payload.message;
+    },
+  },
+  getters: {
+    targetCategory: state => state.targetCategory,
   },
   actions: {
     getAllCategories({ commit, rootGetters }) {
@@ -23,6 +45,34 @@ export default {
         commit('doneGetAllCategories', payload);
       }).catch(err => {
         commit('failRequest', { message: err.message });
+      });
+    },
+    updatedCategory({ commit }, value) {
+      const payload = {
+        targetCategory: value,
+      };
+      commit('updateCategory', payload);
+    },
+    addCategory({ commit, rootGetters }) {
+      return new Promise((resolve, reject) => {
+        commit('clearMessage');
+        commit('toggleLoading');
+
+        const data = new URLSearchParams();
+        data.append('name', rootGetters['categories/targetCategory']);
+        axios(rootGetters['auth/token'])({
+          method: 'POST',
+          url: '/category',
+          data,
+        }).then(() => {
+          commit('toggleLoading');
+          commit('displayDoneMessage', { message: 'カテゴリーを作成しました' });
+          resolve();
+        }).catch(err => {
+          commit('toggleLoading');
+          commit('failRequest', { message: err.message });
+          reject();
+        });
       });
     },
   },
