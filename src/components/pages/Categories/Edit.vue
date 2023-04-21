57365d267a099a76c3ca744379c185e92f228c35
堀家將平 <shohei@horikenoMacBook-Pro.local>
2023-04-21 15:06:55 +0900

    CategoryPost Exportに追加

---
+++ src/components/pages/Categories/Edit.vue
@@ -0,0 +1,17 @@
+<template>
+  <app-category-edit />
+</template>
+
+<script>
+import CategoryEdit from '@Components/molecules';
+
+export default {
+  components: {
+    AppCategoryEdit: CategoryEdit,
+  },
+};
+</script>
+
+<style>
+
+</style>
