cdd7aadc7e13516d21deed29832fcfbfba5e7b8e
Christopher-90 <kilika.10.pher@gmail.com>
2023-04-27 14:34:06 +0900

    feat: カテゴリ一新規作成機能の処理を追加

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -3,10 +3,10 @@ import axios from '@Helpers/axiosDefault';
 export default {
   namespaced: true,
   state: {
+    category: '',
     categoriesList: [],
-    deleteCategoriesList: null,
-    errorMessage: '',
     doneMessage: '',
+    errorMessage: '',
   },
   mutations: {
     failRequest(state, { message }) {
@@ -21,6 +21,9 @@ export default {
     },
   },
   actions: {
+    clearMessage({ commit }) {
+      commit('clearMessage');
+    },
     getCategoryList({ commit, rootGetters }) {
       axios(rootGetters['auth/token'])({
         method: 'GET',
@@ -28,6 +31,29 @@ export default {
       }).then(res => {
         const categories = res.data.categories.reverse();
         commit('setCategories', { categories });
+        commit('clearMessage');
+      }).catch(err => {
+        commit('failRequest', { message: err.message });
+      });
+    },
+    updateCategory({ commit, rootGetters }, categoryName) {
+      axios(rootGetters['auth/token'])({
+        method: 'POST',
+        url: '/category',
+        data: {
+          name: categoryName,
+        },
+      }).then(() => {
+        axios(rootGetters['auth/token'])({
+          method: 'GET',
+          url: '/category',
+        }).then(res => {
+          const categories = res.data.categories.reverse();
+          commit('setCategories', { categories });
+          commit('clearMessage');
+        }).catch(err => {
+          commit('failRequest', { message: err.message });
+        });
         commit('clearMessage');
       }).catch(err => {
         commit('failRequest', { message: err.message });
