a7d9fe58682d06da099d9f1fd187019e148d469e
ta9to <ta9to@TakutonoMacBook-Pro.local>
2023-04-27 12:48:49 +0900

    feat:記事一覧ページにページネーションボタンを追加（表示のみ）

---
+++ src/components/molecules/Pagination/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <ol class="pagination">
+    <li class="pagination-btn">1</li>
+    <li class="pagination-btn pagination-dots">• • •</li>
+    <li class="pagination-btn">now</li>
+    <li class="pagination-btn pagination-dots">• • •</li>
+    <li class="pagination-btn">last</li>
+  </ol>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  text-align: center;
+  margin-top: 30px;
+  &-btn {
+    width: 56px;
+    padding: 10px;
+    margin-left: 20px;
+    color: $white;
+    font-size: 16px;
+    line-height: 24px;
+    background-color: $theme-color;
+    &:first-child {
+      margin-left: 0;
+    }
+  }
+  &-dots {
+    color: $disabled-color;
+    background-color: $white;
+  }
+}
+</style>
