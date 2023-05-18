199387c7ba3a0fcd4ef94ab636e19e8ea77e457d
MACBOOK <3aos4682@gmail.com>
2023-05-18 15:14:20 +0900

    vite.config.jsの不要なコードを削除した

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import legacy from '@vitejs/plugin-legacy';
 import checker from 'vite-plugin-checker';
-// nodemoduleからパッケージインストール
 
 export default (({ mode }) => {
   const rootDirPath = process.cwd();
