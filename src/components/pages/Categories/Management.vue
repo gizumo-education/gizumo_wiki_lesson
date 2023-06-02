2edf8e7950adf0588aaafcf462034d62ad03e278
青木彩香 <>
2023-06-02 19:02:53 +0900

    カテゴリー新規作成機能実装

--- src/components/pages/Categories/Management.vue
+++ src/components/pages/Categories/Management.vue
@@ -3,6 +3,12 @@
     <app-category-post
       :access="access"
       class="category-post"
+      :category="categoryName"
+      :done-message="doneMessage"
+      :error-message="errorMessage"
+      :disabled="loading"
+      @update-value="categoryName = $event.target.value"
+      @handle-submit="postCategory"
     />
     <app-category-list
       :categories="categoryList"
@@ -24,6 +30,7 @@ export default {
   data() {
     return {
       theads: ['カテゴリー名'],
+      categoryName: '',
     };
   },
   computed: {
@@ -33,9 +40,26 @@ export default {
     access() {
       return this.$store.getters['auth/access'];
     },
+    doneMessage() {
+      return this.$store.state.categories.doneMessage;
+    },
+    errorMessage() {
+      return this.$store.state.categories.errorMessage;
+    },
+    loading() {
+      return this.$store.state.categories.loading;
+    },
   },
   created() {
     this.$store.dispatch('categories/getAllCategories');
+    this.$store.dispatch('categories/clearMessage');
+  },
+  methods: {
+    postCategory() {
+      this.$store.dispatch('categories/createCategory', this.categoryName).then(() => {
+        this.categoryName = '';
+      });
+    },
   },
 };
 </script>
