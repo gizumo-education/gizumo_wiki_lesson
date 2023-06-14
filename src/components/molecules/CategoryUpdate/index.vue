cd381daa2fcd239b8bd615b5d8d3fce7d4696fa5
Koki Miyano <tasuki.nagachi@gmail.com>
2023-06-14 17:45:02 +0900

    refactor: アクセス権限の部分をcreateになっていたところをeditへと変更しました！！

--- src/components/molecules/CategoryUpdate/index.vue
+++ src/components/molecules/CategoryUpdate/index.vue
@@ -29,7 +29,7 @@
         class="category-update-info__submit"
         button-type="submit"
         round
-        :disabled="disabled || !access.create"
+        :disabled="disabled || !access.edit"
       >
         {{ buttonText }}
       </app-button>
@@ -82,13 +82,13 @@ export default {
   },
   computed: {
     buttonText() {
-      if (!this.access.create) return '更新権限がありません';
+      if (!this.access.edit) return '更新権限がありません';
       return this.disabled ? '更新中...' : '更新';
     },
   },
   methods: {
     updateCategory() {
-      if (!this.access.create) return;
+      if (!this.access.edit) return;
       this.$emit('clear-message');
       this.$validator.validate().then(valid => {
         if (valid) this.$emit('update-category');
