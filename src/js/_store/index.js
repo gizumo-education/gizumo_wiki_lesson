358915396302ff15274617d5c1d092ce972d2c05
Koki Miyano <tasuki.nagachi@gmail.com>
2023-07-06 12:04:09 +0900

    refactor: vuex-persistedstateプラグインを削除しました！

--- src/js/_store/index.js
+++ src/js/_store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createPersistedState from 'vuex-persistedstate';
 import {
   auth, articles, users, categories,
 } from './modules';
@@ -14,17 +13,4 @@ export default new Vuex.Store({
     users,
     categories,
   },
-  plugins: [createPersistedState(
-    { // ストレージのキーを指定。デフォルトではvuex
-      key: 'vuex',
-
-      // 管理対象のステートを指定。pathsを書かない時は`modules`に書いたモジュールに含まれるステート全て。`[]`の時はどれも保存されない
-      paths: [
-        'articles.pagenation.current_page',
-      ],
-
-      // ストレージの種類を指定する。デフォルトではローカルストレージ
-      storage: window.sessionStorage,
-    },
-  )],
 });
