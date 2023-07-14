ff8276cd391d06072a8542b8db58a075ea8b3c95
Koki Miyano <tasuki.nagachi@gmail.com>
2023-07-14 14:08:25 +0900

    refactor: yyyy-mm-ddの処理を簡略化しました。

--- src/components/molecules/ArticleTrashTable/index.vue
+++ src/components/molecules/ArticleTrashTable/index.vue
@@ -59,10 +59,7 @@ export default {
   methods: {
     formattedDate(targetDate) {
       const date = new Date(targetDate);
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const day = String(date.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}`;
+      return date.toLocaleDateString();
     },
   },
 };
