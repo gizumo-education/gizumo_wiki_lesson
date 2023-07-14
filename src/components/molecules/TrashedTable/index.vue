8a976f783b40e4cc2787df955e6914529b96878c
y-hashimoto-2023 <yukhashimo01@gmail.com>
2023-07-14 12:47:32 +0900

    refactor: toLocaleDateString()を使用

--- src/components/molecules/TrashedTable/index.vue
+++ src/components/molecules/TrashedTable/index.vue
@@ -56,11 +56,7 @@ export default {
   },
   methods: {
     formatDate(date) {
-      const formattedDate = new Date(date);
-      const year = formattedDate.getFullYear();
-      const month = String(formattedDate.getMonth() + 1).padStart(2, '0');
-      const day = String(formattedDate.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day}`;
+      return new Date(date).toLocaleDateString();
     },
   },
 };
