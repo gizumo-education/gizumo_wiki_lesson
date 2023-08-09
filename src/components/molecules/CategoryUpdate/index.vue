d6c543e79ed7f1f6b64e9150eea3fcb2a40c5f77
Tomoyuki Tanak <tomoyukitanaka@TomoyukinoMacBook-Pro.local>
2023-08-09 14:52:49 +0900

    add:カテゴリー更新ページの作成、リンクの追加

---
+++ src/components/molecules/CategoryUpdate/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="category-update">
+    <app-heading :level="1">カテゴリー管理</app-heading>
+    <app-router-link
+      :to="`/categories`"
+      underline
+      hover-opacity
+    >
+      カテゴリー一覧へ戻る
+    </app-router-link>
+    <app-input
+      v-validate="'required'"
+      class="category-update__input"
+      name="category"
+      type="text"
+    />
+    <app-button
+      class="category-update__button"
+      round
+    >
+      更新
+    </app-button>
+  </div>
+</template>
+
+<script>
+import {
+  RouterLink, Heading, Input, Button,
+} from '@Components/atoms';
+
+export default {
+  components: {
+    appRouterLink: RouterLink,
+    appHeading: Heading,
+    appInput: Input,
+    appButton: Button,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.category-update {
+  .router-link {
+    padding-top: 16px;
+  }
+  &__input {
+    margin-top: 16px;
+  }
+  &__button {
+    margin-top: 16px;
+  }
+}
+</style>
---
+++ src/components/pages/Categories/Update.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <app-category-update>
+      class="category-update"
+      :category="targetCategory"
+    </app-category-update>
+  </div>
+</template>
+
+<script>
+import { CategoryUpdate } from '@Components/molecules';
+
+export default {
+  components: {
+    appCategoryUpdate: CategoryUpdate,
+  },
+  created() {
+    this.$store.dispatch('categories/getAllCategories');
+  },
+};
+</script>
