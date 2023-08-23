1055f2b9b118ac71d555bd94c5af318d609b3dd1
yusuke0927 <gorirando0927@gmail.com>
2023-08-23 15:46:44 +0900

    カテゴリーボタンクリックによる遷移画面設定③

--- src/components/pages/Categories/CategoryList.vue
+++ src/components/pages/Categories/CategoryList.vue
@@ -1,15 +1,13 @@
 <template>
   <app-Category-list />
-  <app-Category-Post />
 </template>
 
 <script>
-import { CategoryList, CategoryPost } from '@Components/molecules';
+import { CategoryList } from '@Components/molecules';
 
 export default {
   components: {
     appCategoryList: CategoryList,
-    appCategoryPost: CategoryPost,
   },
 };
 
