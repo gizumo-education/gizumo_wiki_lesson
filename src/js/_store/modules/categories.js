d0a0f9401e135085ad198cf1191a8761221b7b64
NYA1129 <99474626+NYA1129@users.noreply.github.com>
2023-09-12 17:32:30 +0900

    削除機能の実装(conflict解消test)

--- src/js/_store/modules/categories.js
+++ src/js/_store/modules/categories.js
@@ -12,13 +12,10 @@ export default {
     errorMessage: '',
     doneMessage: '',
     loading: false,
-    deleteCategoryId: null,
   },
   getters: {
     targetCategory: state => state.targetCategory,
     categoryList: state => state.categoryList,
-    deleteCategoryId: state => state.deleteCategoryId,
-
   },
   mutations: {
     doneGetAllCategories(state, payload) {
@@ -49,15 +46,6 @@ export default {
     resetView(state) {
       state.doneMessage = '';
       state.errorMessage = '';
-    },
-    confirmDeleteId(state, { categoryId }) {
-      state.deleteCategoryId = categoryId;
-    },
-    doneDeleteCategory(state) {
-      state.deleteCategoryId = null;
-    },
-    deletedCategoryList(state, payload) {
-      return state.categoryList.splice(payload, 1);
     },
   },
   actions: {
